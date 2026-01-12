8ad4c2b907a5decff159ebe19ecfade7a76ea089
SheWantKarma <68813991+SheWantsKarma@users.noreply.github.com>
2026-01-12 13:11:17 +0100

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,38 +4,39 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
-    username: "24ATERNOSBOT",
+    host: "IntellectualSMP1.aternos.me",
+    port: "38222",
+    username: "SMP_BOT",
     version: false
     })
     bot.on('login', function() {
-      bot.chat('/reginster 123123123 123123123')
-      bot.chat('/login 123123123 123123123')
+      bot.chat('Intellectual SMP BOT > SMP_BOT has joined the server! ✔️')
+      bot.chat('Intellectual SMP BOT > Thank you daddy schizo! ✔️')
     })
     bot.on('chat', (username, message) => {
       if (username === bot.username) return
       switch (message) {
         case ';start':
-          bot.chat('24 ATERNOS > Bot started! - Made By Fortcote')
+          bot.chat('Intellectual SMP BOT > Bot started!')
           bot.setControlState('forward', true)
           bot.setControlState('jump', true)
           bot.setControlState('sprint', true)
           break
           case ';stop':
-            bot.chat('24 ATERNOS > Bot stoped! - Made By Fortcote')
+            bot.chat('Intellectual SMP BOT > Bot stoped!')
             bot.clearControlStates()
             break
           }
         })
         bot.on('spawn', function() {
-          bot.chat('Bot > Spawned')
+          bot.chat('Intellectual SMP BOT > Spawned')
         })
         bot.on('death', function() {
-          bot.chat('Bot > I died, respawn')
+          bot.chat('Intellectual SMP BOT > Respawned')
         })
         bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
         bot.on('error', err => console.log(err))
         bot.on('end', createBot)
 }
+
 createBot()
